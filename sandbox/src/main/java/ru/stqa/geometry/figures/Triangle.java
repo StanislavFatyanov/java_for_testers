5408a57c7537669350a140b6a0baf0885acce0f1
Фатьянов Станислав Сергеевич <stanislav.lyashenko@fors.ru>
2025-09-20 17:02:16 +0300

    Доработана функция equals в классе Triangle, реализованы тесты для сравнения треугольников

--- sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
@@ -1,5 +1,7 @@
 package ru.stqa.geometry.figures;
 
+import java.util.Objects;
+
 public record Triangle(double a, double b, double c) {
 
     public Triangle{
@@ -29,5 +31,20 @@ public record Triangle(double a, double b, double c) {
     public double triangleArea() {
         double s = trianglePerimeter() / 2;
         return Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Triangle triangle = (Triangle) o;
+        return Double.compare(a, triangle.a) == 0 && Double.compare(b, triangle.b) == 0 && Double.compare(c, triangle.c) == 0
+        || Double.compare(a, triangle.b) == 0 && Double.compare(b, triangle.c) == 0 && Double.compare(c, triangle.a) == 0
+        || Double.compare(a, triangle.c) == 0 && Double.compare(b, triangle.a) == 0 && Double.compare(c, triangle.b) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return 1;
     }
 }
--- sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
+++ sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
@@ -60,4 +60,25 @@ public class TriangleTests {
             // OK
         }
     }
+
+    @Test
+    void testEquality1(){
+        var t1 = new Triangle(3.0, 4.0, 5.0);
+        var t2 = new Triangle(4.0, 5.0, 3.0);
+        Assertions.assertEquals(t1, t2);
+    }
+
+    @Test
+    void testEquality2(){
+        var t1 = new Triangle(4.0, 5.0, 3.0);
+        var t2 = new Triangle(5.0, 3.0, 4.0);
+        Assertions.assertEquals(t1, t2);
+    }
+
+    @Test
+    void testEquality3(){
+        var t1 = new Triangle(5.0, 3.0, 4.0);
+        var t2 = new Triangle(3.0, 4.0, 5.0);
+        Assertions.assertEquals(t1, t2);
+    }
 }
