2fd135d9b9c19927cb099e447a57fb23189b7cfe
Фатьянов Станислав Сергеевич <stanislav.lyashenko@fors.ru>
2025-09-20 16:20:43 +0300

    Добавлены првоерки на отрицательные стороны треугольника и неравенство сторон треугольника

--- sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
@@ -2,6 +2,15 @@ package ru.stqa.geometry.figures;
 
 public record Triangle(double a, double b, double c) {
 
+    public Triangle{
+        if (a < 0 || b < 0 || c < 0){
+            throw new IllegalArgumentException("Сторона треугольника не должна быть отрицательной");
+        }
+        if ((a + b) < c || (a + c) < b || (b + c) < a){
+            throw new IllegalArgumentException("Cумма двух любых сторон должна быть не меньше третьей стороны");
+        }
+    }
+
 
     public static void printTriangePerimeter(Triangle s) {
         var text = String.format("Периметр треугольника со сторонами %f и %f и %f = %f", s.a, s.b, s.c, s.trianglePerimeter());
--- sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
+++ sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
@@ -6,14 +6,58 @@ import org.junit.jupiter.api.Test;
 public class TriangleTests {
 
     @Test
-    void canCalculatePerimeter(){
+    void canCalculatePerimeter() {
         var s = new Triangle(3.0, 4.0, 5.0);
         double result = s.trianglePerimeter();
         Assertions.assertEquals(12, result);
     }
 
     @Test
-    void canCalculateArea(){
+    void canCalculateArea() {
         Assertions.assertEquals(6.0, new Triangle(3.0, 4.0, 5.0).triangleArea());
+    }
+
+    @Test
+    void cannotCreateTriangleWithNegativeSide() {
+        try {
+            new Triangle(-3.0, 4.0, 5.0);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            // OK
+        }
+        try {
+            new Triangle(3.0, -4.0, 5.0);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            // OK
+        }
+        try {
+            new Triangle(3.0, 4.0, -5.0);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            // OK
+        }
+    }
+
+    @Test
+    void cannotCreateTriangkeWithNotValidSides(){
+        try {
+            new Triangle(1.0, 3.0, 5.0);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            // OK
+        }
+        try {
+            new Triangle(1.0, -4.0, 2.0);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            // OK
+        }
+        try {
+            new Triangle(6.0, 3.0, 2.0);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            // OK
+        }
     }
 }
