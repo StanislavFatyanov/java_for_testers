875e13988a9976406e58326f4768b24ea5978069
Фатьянов Станислав Сергеевич <stanislav.lyashenko@fors.ru>
2025-09-22 10:37:51 +0300

    Доработан тест создания группы, добавлен тест на удаление группы

---
+++ addressbook_web_tests/src/test/java/GroupRemovalTests.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class GroupRemovalTests {
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        driver = new FirefoxDriver();
+        driver.get("http://localhost/addressbook/index.php");
+        driver.manage().window().setSize(new Dimension(1400, 816));
+        driver.findElement(By.name("user")).click();
+        driver.findElement(By.name("user")).sendKeys("admin");
+        driver.findElement(By.name("pass")).click();
+        driver.findElement(By.name("pass")).sendKeys("secret");
+        driver.findElement(By.cssSelector("input:nth-child(7)")).click();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.findElement(By.linkText("Logout")).click();
+        driver.quit();
+    }
+
+    @Test
+    public void canRemoveGroup() {
+        driver.findElement(By.linkText("groups")).click();
+        driver.findElement(By.name("selected[]")).click();
+        driver.findElement(By.name("delete")).click();
+        driver.findElement(By.linkText("group page")).click();
+    }
+}
