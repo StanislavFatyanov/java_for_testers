16ee9ac9782264dbd297f04fe5b08dbe574cd7af
Фатьянов Станислав Сергеевич <stanislav.lyashenko@fors.ru>
2025-11-04 21:19:35 +0300

    Создан новый модуль для нового учебного приложения, добавлен конфигурационный файл гредл, создана структура

---
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
@@ -0,0 +1,53 @@
+package ru.stqa.mantis.manager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.Properties;
+
+public class ApplicationManager {
+
+    private WebDriver driver;
+    protected WebDriverWait wait;
+    private String string;
+    private Properties properties;
+    private SessionHelper sessionHelper;
+
+    public void init(String browser, Properties properties) {
+        this.string = browser;
+        this.properties = properties;
+        
+    }
+
+    public WebDriver driver() {
+        if (driver == null){
+            if ("firefox".equals(string)) {
+                driver = new FirefoxDriver();
+            } else if ("chrome".equals(string)) {
+                driver = new ChromeDriver();
+            } else if ("safari".equals(string)) {
+                driver = new SafariDriver();
+            } else {
+                throw new IllegalArgumentException(String.format("Unknow browser %f", string));
+            }
+            this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
+            driver.get(properties.getProperty("web.baseUrl"));
+            driver.manage().window().setSize(new Dimension(1400, 816));
+        }
+        return driver;
+    }
+
+    public SessionHelper sessionHelper(){
+        if (sessionHelper == null){
+            sessionHelper = new SessionHelper(this);
+        }
+        return sessionHelper;
+    }
+}
---
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/HelperBase.java
@@ -0,0 +1,40 @@
+package ru.stqa.mantis.manager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.nio.file.Paths;
+
+public class HelperBase {
+    protected final ApplicationManager manager;
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    public HelperBase(ApplicationManager manager) {
+
+        this.manager = manager;
+        this.driver = manager.driver();
+        this.wait = manager.wait;
+    }
+
+    protected void type(By locator, String text) {
+        click(locator);
+        manager.driver().findElement(locator).clear();
+        manager.driver().findElement(locator).sendKeys(text);
+    }
+
+    protected void attach(By locator, String file) {
+        manager.driver().findElement(locator).sendKeys(Paths.get(file).toAbsolutePath().toString());
+    }
+
+    protected void click(By locator) {
+        wait.until(ExpectedConditions.elementToBeClickable(locator)).click();
+        //manager.driver.findElement(locator).click();
+    }
+
+    protected boolean isElementPresent(By locator){
+       return manager.driver().findElements(locator).size() > 0;
+    }
+}
