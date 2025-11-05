1196e3e0d4ba9dff19f79d32f4bae0a0922ae172
Фатьянов Станислав Сергеевич <stanislav.lyashenko@fors.ru>
2025-11-05 08:55:14 +0300

    Логика теста логина переписана на работу по http

--- mantis_tests/src/test/java/ru/stqa/mantis/tests/LoginTests.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/tests/LoginTests.java
@@ -7,7 +7,7 @@ public class LoginTests extends TestBase {
 
     @Test
     void canLogin(){
-      app.sessionHelper().login("administrator", "root");
-        Assertions.assertTrue(app.sessionHelper().isLoggedIn());
+      app.http().login("administrator", "root");
+        Assertions.assertTrue(app.http().isLoggedIn());
     }
 }
