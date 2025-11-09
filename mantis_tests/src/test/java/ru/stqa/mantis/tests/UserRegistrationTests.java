93b07eb4d0635991140b6977fad19f2ead8dcb6b
Фатьянов Станислав Сергеевич <stanislav.lyashenko@fors.ru>
2025-11-09 12:36:08 +0300

    Реализация теста регистрации пользователя с созданием почты и польщователя через апи

--- mantis_tests/src/test/java/ru/stqa/mantis/tests/UserRegistrationTests.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/tests/UserRegistrationTests.java
@@ -3,6 +3,8 @@ package ru.stqa.mantis.tests;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.stqa.mantis.common.CommonFunctions;
+import ru.stqa.mantis.model.IssueData;
+import ru.stqa.mantis.model.UserData;
 
 import java.time.Duration;
 import java.util.regex.Pattern;
@@ -23,6 +25,30 @@ public class UserRegistrationTests extends TestBase{
         String url = null;
         if (matcher.find()){
            url = text.substring(matcher.start(), matcher.end());
+        }
+        app.driver().get(url);
+        app.session().finishedRegistration(user, "password");
+        app.http().login(user, "password");
+        Assertions.assertTrue(app.http().isLoggedIn());
+    }
+
+    @Test
+    void canRegisterUserApi (){
+        var user = CommonFunctions.randomString(8);
+        var email = String.format("%s@localhost", user);
+        app.jamesApi().addUser(email, "password");
+        app.rest().addNewUser(new UserData()
+                .withUserName(user)
+                .withPassword("password")
+                .withEmail(email)
+                .withAccessLevel("reporter"));
+        var messages = app.mail().receive(email, "password", Duration.ofSeconds(60));
+        var text = messages.get(0).content();
+        var pattern = Pattern.compile("http://\\S*");
+        var matcher = pattern.matcher(text);
+        String url = null;
+        if (matcher.find()){
+            url = text.substring(matcher.start(), matcher.end());
         }
         app.driver().get(url);
         app.session().finishedRegistration(user, "password");
