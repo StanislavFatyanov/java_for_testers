cb609cd85c2376ce6a98d583815b1e08448db5a6
Фатьянов Станислав Сергеевич <stanislav.lyashenko@fors.ru>
2025-11-16 18:20:00 +0300

    Добавление настроек для запуска тестов на сервере селениума

--- addressbook_web_tests/src/test/java/manager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/manager/ApplicationManager.java
@@ -5,10 +5,16 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.safari.SafariOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 import java.util.Properties;
 
@@ -24,15 +30,28 @@ public class ApplicationManager {
 
     private Properties properties;
 
-    public void init(String browser, Properties properties) {
+    public void init(String browser, Properties properties) throws MalformedURLException {
         this.properties = properties;
         if (driver == null) {
+            var seleniumServer = properties.getProperty("seleniumServer");
             if ("firefox".equals(browser)) {
-                driver = new FirefoxDriver();
+                if (seleniumServer != null){
+                    driver = new RemoteWebDriver(new URL(seleniumServer), new FirefoxOptions());
+                } else {
+                    driver = new FirefoxDriver();
+                }
             } else if ("chrome".equals(browser)) {
-                driver = new ChromeDriver();
+                if (seleniumServer != null){
+                    driver = new RemoteWebDriver(new URL(seleniumServer), new ChromeOptions());
+                } else {
+                    driver = new ChromeDriver();
+                }
             } else if ("safari".equals(browser)) {
-                driver = new SafariDriver();
+                    if (seleniumServer != null){
+                        driver = new RemoteWebDriver(new URL(seleniumServer), new SafariOptions());
+                    } else {
+                        driver = new SafariDriver();
+                }
             } else {
                 throw new IllegalArgumentException(String.format("Unknow browser %f", browser));
             }
